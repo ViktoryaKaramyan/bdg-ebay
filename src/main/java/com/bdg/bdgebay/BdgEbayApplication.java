9781820f830b056819888936c3dc7ac1c415b538
William Arustamyan <williamarustamyan@gmail.com>
2019-04-04 16:33:21 +0400

    BdgEbay web Application initial commit.

---
+++ src/main/java/com/bdg/bdgebay/BdgEbayApplication.java
@@ -0,0 +1,13 @@
+package com.bdg.bdgebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdgEbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdgEbayApplication.class, args);
+	}
+
+}
