79d5299d6790f8b9b4294cac98313862d2c5f865
William Arustamyan <williamarustamyan@workfront.com>
2019-04-04 18:29:30 +0400

    Initial domain and service implementation

--- src/main/java/com/bdg/bdgebay/BdgEbayApplication.java
+++ src/main/java/com/bdg/bdgebay/BdgEbayApplication.java
@@ -2,8 +2,10 @@ package com.bdg.bdgebay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+//@PropertySource("classpath:repository.properties")
 public class BdgEbayApplication {
 
 	public static void main(String[] args) {
