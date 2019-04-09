f6902b36eeac0296e9a03c7700b3695c93c1868d
Vika <vika.karamyan@gmail.com>
2019-04-09 23:27:01 +0400

    CRUD for User

--- src/main/java/com/bdg/bdgebay/controller/user/UserController.java
+++ src/main/java/com/bdg/bdgebay/controller/user/UserController.java
@@ -1,9 +1,16 @@
 package com.bdg.bdgebay.controller.user;
 
 import com.bdg.bdgebay.dto.user.UserCreationRequest;
+import com.bdg.bdgebay.dto.user.UserUpdateRequest;
 import com.bdg.bdgebay.entity.User;
+import com.bdg.bdgebay.service.user.UserService;
+import com.bdg.bdgebay.service.user.UserServiceImpl;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,14 +23,32 @@ public class UserController {
 
     private ModelMapper modelMapper;
 
+    @Autowired
+    private UserService userService;
+
 
     //make this method endpoint
-    public User createUser(UserCreationRequest creationRequest) {
-        System.out.println(creationRequest);
-        return new User();
+    @RequestMapping(path = "/user/create", method = RequestMethod.POST)
+    public User createUser(@RequestBody UserCreationRequest creationRequest) {
+        return userService.create(creationRequest);
     }
 
+    @RequestMapping(path = "/user/{id}/update", method =  RequestMethod.PUT)
+    public User updateUser(@RequestBody UserUpdateRequest updateRequest) {
+    	
+		return userService.update(updateRequest);
+    	
+    }
+    
+    @RequestMapping(path = "/user/{id}/delete", method = RequestMethod.DELETE)
+    public User deleteUser(@RequestBody UserDeleteRequest deleteRequest) {
+    	
+    	return userService.delete(deleteRequest);
+    }
 
+    @RequestMapping(path = "/user/{id}", method = RequestMethod.GET)
+    public User getUser()
+    
     @Autowired
     public void setModelMapper(final ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
--- src/main/java/com/bdg/bdgebay/service/user/UserServiceImpl.java
+++ src/main/java/com/bdg/bdgebay/service/user/UserServiceImpl.java
@@ -4,53 +4,75 @@ import com.bdg.bdgebay.dto.user.UserCreationRequest;
 import com.bdg.bdgebay.dto.user.UserUpdateRequest;
 import com.bdg.bdgebay.entity.User;
 import com.bdg.bdgebay.repository.user.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 //import javax.persistence.EntityManager;
 //import javax.persistence.PersistenceContext;
 
 /**
- * @author William Arustamyan
- */
+* @author William Arustamyan
+*/
 @Service
 public class UserServiceImpl implements UserService {
 
 
-    private UserRepository userRepository;
+  private UserRepository userRepository;
 
-//    @PersistenceContext
-//    private EntityManager entityManager;
+//  @PersistenceContext
+//  private EntityManager entityManager;
 
-    @Override
-    public User get(final Long userId) {
-        return userRepository.findByIdAndDeletedIsNull(userId);
-    }
+  @Override
+  public User get(final Long userId) {
+      return userRepository.findByIdAndDeletedIsNull(userId);
+  }
 
-    @Override
-    public User create(UserCreationRequest creationRequest) {
+  @Override
+  public User create(final UserCreationRequest creationRequest) {
+      final User user = new User();
+      user.setCreated(LocalDateTime.now());
+      user.setPassword(creationRequest.getPassword());
+      user.setUserName(creationRequest.getUserName());
+      user.setLastName(creationRequest.getLastName());
+      user.setFirstName(creationRequest.getFirstName());
+      user.setEmailAddress(creationRequest.getEmailAddress());
+      return user;
+  }
 
-        //Implement this
+  @Override
+  public User delete(Long userId) {
+	return null;
+      //Implement this (use userRepository)
+	  
+  }
 
-        return null;
-    }
+  @Override
+  public User update(UserUpdateRequest updateRequest) {
 
-    @Override
-    public User delete(Long userId) {
-        //Implement this (use userRepository)
+      //Implement this (at first need to get user from db after do save)
+	  User user = userRepository.findByIdAndDeletedIsNull(updateRequest.getUserId());
+	  user.setUpdated(LocalDateTime.now());
+	  user.setPassword(updateRequest.getPassword());
+      user.setUserName(updateRequest.getUserName());
+      user.setId(updateRequest.getUserId());
+      
+      return user;
+  }
 
-        return null;
-    }
-
-    @Override
-    public User update(UserUpdateRequest updateRequest) {
-
-        //Implement this (at first need to get user from db after do save)
-        return null;
-    }
-
-//    @Autowired
-//    public void setUserRepository(final UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
+//  @Autowired
+//  public void setUserRepository(final UserRepository userRepository) {
+//      this.userRepository = userRepository;
+//  }
 }
